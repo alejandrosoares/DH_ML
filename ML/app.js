b74515d562fdba2799bb157cd189fd0f65403081
aleyacare <alejandro.soares@yacare.com>
2022-06-01 20:38:54 -0300

    ops: agregando configuraciones de deploy

--- ML/app.js
+++ ML/app.js
@@ -5,11 +5,13 @@ const bodyParser = require('body-parser')
 const publicPath = path.resolve(__dirname, 'public')
 const app = express()
 
+// MIDDLEWARE
 app.use(express.static(publicPath))
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.urlencoded({ extended: true }))
+app.set('PORT', process.env.PORT || 3000)
 
-app.listen(3000, () => {
-    console.log('Servidor corriendo en puerto 3000')
+app.listen(app.get('PORT'), () => {
+    console.log('Servidor corriendo en puerto ' + app.get('PORT'))
 })
 
 app.get('/', (req, res) => {
